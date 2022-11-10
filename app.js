94a72854a2d0c95b48cea0ad07cb0e5aa95d7157
Brian Etaghene <joveee05@gmail.com>
2022-11-10 01:48:01 +0100

    Included Global Error Handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const globalErrorHandler = require('./controllers/errorController');
 const cors = require('cors');
 const AppError = require('./utils/appError');
 const userRouter = require('./routes/userRoutes');
@@ -17,5 +18,7 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 app.use('/api/v1/users/', userRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
