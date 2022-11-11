f4c5a814b5c8b5e2f54af493c7ba10bac8729f4d
Brian Etaghene <joveee05@gmail.com>
2022-11-11 02:49:28 +0100

    Made it Better

--- create_transactions_table.js
+++ create_transactions_table.js
@@ -4,9 +4,8 @@ knex.schema
   .createTable('transactions', (table) => {
     table.increments('id');
     table.string('transactionType').notNullable();
-    table.integer('user_id').notNullable().unique();
     table.integer('account_id').notNullable().unique();
-    table.string('amount').notNullable();
+    table.float('amount').notNullable();
     table.timestamps(true, true);
   })
   .then(() => console.log('transactions table created'))
